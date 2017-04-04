3be2bb1bdd97f2309c66d734a4fa013dbe5d510b
Pieter-Jan Drouillon <Pieter-Jan.Drouillon@telenetgroup.be>
2017-04-04 07:53:58 +0200

    initial commit.

---
+++ bundle/src/main/java/com/mycompany/myproject/AdobeCQTicket115263/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.mycompany.myproject.AdobeCQTicket115263;
+
+import aQute.bnd.annotation.Version;
---
+++ bundle/src/test/java/com/mycompany/myproject/AdobeCQTicket115263/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.myproject.AdobeCQTicket115263;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
